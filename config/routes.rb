8bcebd22fad35028189494f0cb30b654d27d4ab3
D-Black-N <dn4sibullin@yandex.ru>
2021-11-25 12:49:01 +0300

    Realise

--- config/routes.rb
+++ config/routes.rb
@@ -12,28 +12,5 @@ Rails.application.routes.draw do
   post 'login', to: 'session#create'       # создание новой сессии 
   delete 'session/destroy'                 # выход из сессии
 
-  direct :image do |model, options|
-    if model.respond_to?(:signed_id)
-      route_for(
-        :rails_service_blob_proxy,
-        model.signed_id,
-        model.filename,
-        options.merge(host: ENV['CDN_HOST'])
-      )
-    else
-      signed_blob_id = model.blob.signed_id
-      variation_key  = model.variation.key
-      filename       = model.blob.filename
-  
-      route_for(
-        :rails_blob_representation_proxy,
-        signed_blob_id,
-        variation_key,
-        filename,
-        options.merge(host: ENV['CDN_HOST'])
-      )
-    end
-  end
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
